0e4f37a059a707c549e6e20c17401e377f490a8b
Joshua Steward <joshua.steward@students.fhu.edu>
2018-05-08 11:38:18 -0500

    work

---
+++ AdventureGame/AdventureGame/PlayerData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class PlayerData
+    {
+        public static string Name { get; set; }
+        private static int health;
+        public static int Health {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+                if(health > MaxHealth)
+                {
+                    health = MaxHealth;
+                }
+            }
+        }
+        public static int MaxHealth { get; set; }
+    }
+}
